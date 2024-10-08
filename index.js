a1ab87ee76393ffd52ec7aa9592b8610a4382d58
Tristan Tritch <ttmeep101@hotmail.com>
2024-10-08 10:55:05 -0500

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(highlight, text){
+    const special = text;
+    return function innerFunction(special){
+        return `You are ${highlight}${special}${highlight}!`;
+    }
+}
